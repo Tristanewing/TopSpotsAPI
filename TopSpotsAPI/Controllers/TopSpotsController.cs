89248302a987b8448fbe934a0778e13697e20abd
Tristanewing <tewing@pvs.org>
2016-10-27 09:26:40 -0700

    first commit

---
+++ TopSpotsAPI/Controllers/TopSpotsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TopSpotsAPI.Models;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace TopSpotsAPI.Controllers
+{
+    public class TopSpotsController : ApiController
+    {
+        private JsonConverter[] cProjects;
+
+        public IEnumerable<TopSpot> Get()
+        {
+            var data = JsonConvert.DeserializeObject<List<TopSpot>>(File.ReadAllText("C:\\cProjects\\dev\\TopSpotsAPI\\TopSpotsAPI\\App_Data\\TopSpots.json"));
+            return data;
+        }
+    }
+}
---
+++ TopSpotsAPI/Models/TopSpot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TopSpotsAPI.Models
+{
+    public class TopSpot
+    {
+        //Primary key
+        public string name{ get;set; }
+        public string Description { get; set; }
+        public double[] Location{ get; set; }
+       
+    }
+}
